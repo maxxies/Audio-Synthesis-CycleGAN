703b1bd916013734d6b03a010d129fc9483db570
maxxies <ahiamadzormaxwell7@gmail.com>
2023-07-05 01:45:22 +0000

    Set parameters for anger training

--- params/parameters.py
+++ params/parameters.py
@@ -13,7 +13,7 @@ audio_sampling_rate = 16000
 #############################
 # Model Parameters
 #############################
-model_prefix = 'sad'
+model_prefix = 'anger'
 model_dir =f"/home/jupyter/model/{model_prefix}"
 
 #############################
